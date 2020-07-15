b71787188e4ad7f9b7d88575b73a65dde373894f
Roderic Campbell <roderic@gmail.com>
2020-07-15 11:19:11 -0700

    Update package to use 10.15

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "S3Kit",
-    platforms: [.macOS(.v10_13)],
+    platforms: [.macOS(.v10_15)],
     products: [
         .library(name: "S3Kit", targets: ["S3Kit"]),
         .library(name: "S3Signer", targets: ["S3Signer"]),
